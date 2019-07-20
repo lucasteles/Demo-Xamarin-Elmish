a77220ced4583f7de8cd6b7d4b16f122604efe45
Lucas Teles <lucas.teles@lambda3.com.br>
2019-07-20 10:29:55 -0300

    2

--- FullElmishDemo/Sample/Sample.fs
+++ FullElmishDemo/Sample/Sample.fs
@@ -6,26 +6,22 @@ open Xamarin.Forms
 
 module App =
     type Model =
-      { Count : int
-        Step : int
-      }
+      { Count : int }
 
     type Msg =
         | Increment
         | Decrement
         | Reset
-        | SetStep of int
 
-    let initModel = { Count = 0; Step = 1 }
+    let initModel = { Count = 0 }
 
     let init () = initModel, Cmd.none
 
     let update msg model =
         match msg with
-        | Increment -> { model with Count = model.Count + model.Step }, Cmd.none
-        | Decrement -> { model with Count = model.Count - model.Step }, Cmd.none
+        | Increment -> { model with Count = model.Count + 1 }, Cmd.none
+        | Decrement -> { model with Count = model.Count - 1 }, Cmd.none
         | Reset -> init ()
-        | SetStep n -> { model with Step = n }, Cmd.none
 
     let view (model: Model) dispatch =
         View.ContentPage(
@@ -34,8 +30,6 @@ module App =
                 View.Label(text = sprintf "%d" model.Count, horizontalOptions = LayoutOptions.Center, widthRequest=200.0, horizontalTextAlignment=TextAlignment.Center)
                 View.Button(text = "Increment", command = (fun () -> dispatch Increment), horizontalOptions = LayoutOptions.Center)
                 View.Button(text = "Decrement", command = (fun () -> dispatch Decrement), horizontalOptions = LayoutOptions.Center)
-                View.Slider(minimum = 0.0, maximum = 10.0, value = double model.Step, valueChanged = (fun args -> dispatch (SetStep (int (args.NewValue + 0.5)))), horizontalOptions = LayoutOptions.FillAndExpand)
-                View.Label(text = sprintf "Step size: %d" model.Step, horizontalOptions = LayoutOptions.Center)
                 View.Button(text = "Reset", horizontalOptions = LayoutOptions.Center, command = (fun () -> dispatch Reset), canExecute = (model <> initModel))
             ]))
 
