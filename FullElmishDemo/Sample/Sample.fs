6fc3722ca30aaf5af0ad74aaedebc8cde14b8156
Lucas Teles <lucas.teles@lambda3.com.br>
2019-07-20 10:26:18 -0300

    1

--- FullElmishDemo/Sample/Sample.fs
+++ FullElmishDemo/Sample/Sample.fs
@@ -8,24 +8,17 @@ module App =
     type Model =
       { Count : int
         Step : int
-        TimerOn: bool }
+      }
 
     type Msg =
         | Increment
         | Decrement
         | Reset
         | SetStep of int
-        | TimerToggled of bool
-        | TimedTick
 
-    let initModel = { Count = 0; Step = 1; TimerOn=false }
+    let initModel = { Count = 0; Step = 1 }
 
     let init () = initModel, Cmd.none
-
-    let timerCmd =
-        async { do! Async.Sleep 1000
-                return TimedTick }
-        |> Cmd.ofAsyncMsg
 
     let update msg model =
         match msg with
@@ -33,12 +26,6 @@ module App =
         | Decrement -> { model with Count = model.Count - model.Step }, Cmd.none
         | Reset -> init ()
         | SetStep n -> { model with Step = n }, Cmd.none
-        | TimerToggled on -> { model with TimerOn = on }, (if on then timerCmd else Cmd.none)
-        | TimedTick ->
-            if model.TimerOn then
-                { model with Count = model.Count + model.Step }, timerCmd
-            else
-                model, Cmd.none
 
     let view (model: Model) dispatch =
         View.ContentPage(
@@ -47,8 +34,6 @@ module App =
                 View.Label(text = sprintf "%d" model.Count, horizontalOptions = LayoutOptions.Center, widthRequest=200.0, horizontalTextAlignment=TextAlignment.Center)
                 View.Button(text = "Increment", command = (fun () -> dispatch Increment), horizontalOptions = LayoutOptions.Center)
                 View.Button(text = "Decrement", command = (fun () -> dispatch Decrement), horizontalOptions = LayoutOptions.Center)
-                View.Label(text = "Timer", horizontalOptions = LayoutOptions.Center)
-                View.Switch(isToggled = model.TimerOn, toggled = (fun on -> dispatch (TimerToggled on.Value)), horizontalOptions = LayoutOptions.Center)
                 View.Slider(minimum = 0.0, maximum = 10.0, value = double model.Step, valueChanged = (fun args -> dispatch (SetStep (int (args.NewValue + 0.5)))), horizontalOptions = LayoutOptions.FillAndExpand)
                 View.Label(text = sprintf "Step size: %d" model.Step, horizontalOptions = LayoutOptions.Center)
                 View.Button(text = "Reset", horizontalOptions = LayoutOptions.Center, command = (fun () -> dispatch Reset), canExecute = (model <> initModel))
